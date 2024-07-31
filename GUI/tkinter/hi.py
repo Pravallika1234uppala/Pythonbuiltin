9d6cbcd670d7de6da32c60a7a6f237dfce797557
Pravallika Uppala <123845793+Pravallika1234uppala@users.noreply.github.com>
2024-07-30 23:16:31 -0500

    Add files via upload

---
+++ GUI/tkinter/hi.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+#step-1: Creating a kinter window
+root = Tk()
+
+#Creating a Label widget
+myLabel = Label(root, text="Hi, Pravallika!")
+
+#Shoving it onto the screen
+myLabel.pack()
+
+#Ending the Loop/Program
+root.mainloop()
